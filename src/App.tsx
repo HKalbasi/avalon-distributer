207330fafba338a46e2ba59eba0202619efefd29
hkalbasi <hamidrezakalbasi@protonmail.com>
2025-02-14 23:45:21 +0330

    Add game logic

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,160 @@ const hashCode = (x: string) => {
     hash = ((hash << 5) - hash) + chr;
     hash |= 0; // Convert to 32bit integer
   }
-  return Math.abs(hash).toString(16);
+  return hash;
+}
+
+const splitmix32 = (a: number) => {
+  return () => {
+    a |= 0;
+    a = a + 0x9e3779b9 | 0;
+    let t = a ^ a >>> 16;
+    t = Math.imul(t, 0x21f0aaad);
+    t = t ^ t >>> 15;
+    t = Math.imul(t, 0x735a2d97);
+    return ((t = t ^ t >>> 15) >>> 0) / 4294967296;
+  }
+};
+
+type Game = {
+  roles: string[];
+  starter: number;
+}
+
+
+const rolesPerPlayerCount: { [x: number]: string[] | undefined } = {
+  6: [
+    "Merlin",
+    "Persival",
+    "Mordred",
+    "Morgana",
+    "Servant",
+    "Servant",
+  ],
+  7: [
+    "Merlin",
+    "Persival",
+    "Mordred",
+    "Morgana",
+    "Assassin",
+    "Servant",
+    "Servant",
+  ],
+  8: [
+    "Merlin",
+    "Persival",
+    "Mordred",
+    "Morgana",
+    "Assassin",
+    "Servant",
+    "Servant",
+    "Servant",
+  ],
+  9: [
+    "Merlin",
+    "Persival",
+    "Mordred",
+    "Morgana",
+    "Assassin",
+    "Servant",
+    "Servant",
+    "Servant",
+    "Servant",
+  ],
+  10: [
+    "Merlin",
+    "Persival",
+    "Mordred",
+    "Morgana",
+    "Assassin",
+    "Minion",
+    "Servant",
+    "Servant",
+    "Servant",
+    "Servant",
+  ],
+}
+
+const bads = ["Mordred", "Morgana", "Assassin", "Minion"];
+
+const gameFromSeed = (seed: number, n: number): Game | undefined => {
+  const roles = rolesPerPlayerCount[n];
+  if (!roles) {
+    return;
+  }
+  const rng = splitmix32(seed);
+  return {
+    roles: roles.map(code => ({ code, order: rng() })).sort((a, b) => a.order - b.order).map(x => x.code),
+    starter: Math.floor(rng() * n),
+  }
+}
+
+const renderGameFor = (me: number, players: string[], game: Game) => {
+  const isBad = bads.includes(game.roles[me]);
+  return (
+    <div>
+      <div>
+        You are: {game.roles[me]}
+      </div>
+      {isBad && <div>
+        Your team: <ul>
+          {game.roles.map((role, i) => (
+            bads.includes(role) && <li key={i}>
+              {players[i]}: {role}
+            </li>
+          ))}
+        </ul>
+      </div>}
+      {game.roles[me] === "Merlin" && <div>
+        Bads you know: <ul>
+          {game.roles.map((role, i) => (
+            bads.includes(role) && role !== "Mordred" && <li key={i}>
+              {players[i]}
+            </li>
+          ))}
+        </ul>
+      </div>}
+      {game.roles[me] === "Persival" && <div>
+        You know: <ul>
+          {game.roles.map((role, i) => (
+            (role === "Merlin" || role === "Morgana") && <li key={i}>
+              {players[i]}
+            </li>
+          ))}
+        </ul>
+        As merlin and morgana but you don't know kodoom kodoome
+      </div>}
+      {game.roles[me] === "Servant" && <div>
+        You know nothing, but pretend you are reading name of your team or
+        name of bads or name of merlin and morgana.
+      </div>}
+      <div>
+        Don't look at faces immediately
+      </div>
+      <div>
+        Starter: {players[game.starter]}
+      </div>
+    </div>
+  )
+};
+
+function makeid(length: number) {
+  let result = '';
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
 
 function App() {
   const me = "hamid";
 
   const [friends, setFriends] = useState(JSON.parse(localStorage.getItem('friends') ?? "[]") as Array<Friend>)
+  const [seed, setSeed] = useState("");
 
   const setFriendsPermanent = (x: Friend[]) => {
     localStorage.setItem('friends', JSON.stringify(x));
@@ -32,6 +179,10 @@ function App() {
 
   // sha256 of players to ensure game is same for every one
   const playersHash = hashCode(players.join('#'));
+
+  const seedHash = hashCode(seed);
+
+  const game = gameFromSeed(seedHash, players.length);
 
   return (
     <>
@@ -59,13 +210,17 @@ function App() {
       </div>
       <div>
         Current game:
-        <br/>
+        <br />
         Number of players: {players.length}
-        <br/>
+        <br />
         Players: {players.join(', ')}
-        <br/>
-        Hash of players: {playersHash}
+        <br />
+        Hash of players: {Math.abs(playersHash).toString(16)}
+        <br />
+        <span onClick={() => setSeed(makeid(4))}>Game seed:</span> 
+        <input type="text" value={seed} onChange={e => setSeed(e.target.value)} />
       </div>
+      {game && renderGameFor(players.findIndex((x) => x === me), players, game)}
       <PWABadge />
     </>
   )
