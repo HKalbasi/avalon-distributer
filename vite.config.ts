9f60c0a336abe5b4aef0c06085622cfdc5e0ba2e
S.A.M. <90914468+SAMoosavi@users.noreply.github.com>
2025-07-17 15:09:20 +0330

    Add Winner Information to Exported Game Data (#14)
    * how is winner of the game?
    
    * remove alert
    
    * rename winner

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +11,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: false,
