381bec369195128ee17907e1648aa789d3a95796
SAM <moosavi238@gmail.com>
2025-09-03 18:47:46 +0330

    remove comment

--- src/hooks/useGameLogic.tsx
+++ src/hooks/useGameLogic.tsx
@@ -20,24 +20,3 @@ export const useGameLogic = (players: string[], gameType: GameType, seed: string
 
   return { playersHash, game }
 }
-/*
-import { useMemo } from 'react'
-import { GameType } from '../types'
-import { hashCode, gameFromSeed } from '../utils/gameUtils'
-import { rolesPerPlayerCountAvalon, rolesPerPlayerCountHitler } from '../utils/constants'
-
-export const useGameLogic = (players: string[], gameType: GameType, seed: string) => {
-  const playersHash = useMemo(() => hashCode(players.join('#')), [players])
-  const seedHash = useMemo(() => hashCode(seed) + playersHash, [seed, playersHash])
-
-  const game = useMemo(() => {
-    const rolesPerPlayerCount = gameType === 'Avalon' ? rolesPerPlayerCountAvalon : rolesPerPlayerCountHitler
-    if (players.length in rolesPerPlayerCount) {
-      return gameFromSeed(seedHash, players.length, rolesPerPlayerCount[players.length]!)
-    }
-    return undefined
-  }, [seedHash, players.length, gameType])
-
-  return { playersHash, game }
-}
-  */
